10ab8bed494cf8aa38836c93d118298d048ed694
rayout0823 <86870126+rayout0823@users.noreply.github.com>
2025-12-11 21:01:55 +0800

    Initial commit for M2_Activity2

---
+++ src/ph/com/bpi/Seatwork2.java
@@ -0,0 +1,11 @@
+package ph.com.bpi;
+
+public class Seatwork2 {
+
+    public static void main(String[] args) {
+        Whiteboard wb = new Whiteboard(); 
+        int sum = wb.addNumbers(5, 3);    
+        System.out.println("The total is: " + sum);
+    }
+
+}
---
+++ src/ph/com/bpi/Whiteboard.java
@@ -0,0 +1,10 @@
+package ph.com.bpi;
+
+public class Whiteboard {
+
+	int addNumbers(int addend1, int addend2) {
+		int sum = addend1 + addend2;
+		return sum;
+	}
+
+}
