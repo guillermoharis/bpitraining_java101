866ec7e8c31112a2d3802d0d43f3dd7efab077e0
Sherwin P. Waje <sherwin.p.waje@live.com>
2025-12-15 17:07:57 +0800

    Added project files for M2Group3Project

---
+++ Book.java
@@ -0,0 +1,16 @@
+package ph.com.bpi.hello.group.project2;
+
+public class Book {
+	
+	// adding this "id" to give you an idea on what options you can do
+	private Integer id;
+	private String title;
+	private String author;
+	// feel free to add fields that may help
+	
+	public Integer getId() {
+		return id;
+	}
+	
+ 
+}
---
+++ Loan.java
@@ -0,0 +1,11 @@
+package ph.com.bpi.hello.group.project2;
+
+public class Loan {
+
+
+	private User user;
+	private Book book;
+	
+
+	
+}
