abaa69e2510ec5d28301430acf426b5727ab24bd
rayout0823 <86870126+rayout0823@users.noreply.github.com>
2025-12-18 17:55:39 +0800

    Initial commit for M3_Group3Activity1

---
+++ ViewTotalBill.java
@@ -0,0 +1,10 @@
+package ph.com.bpi.hello.group.project3;
+
+public class ViewTotalBill {
+	
+
+    
+    public static void viewTotalBill(double totalBill) {
+        System.out.println("Total Bill so far: Php" + totalBill);
+    }
+}
